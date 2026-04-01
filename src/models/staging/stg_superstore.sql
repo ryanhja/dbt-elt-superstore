69a5cf9bd962f4c39969773deebed707afaf7a1d
ryanhja <rayanstive9@gmail.com>
2026-04-01 21:24:12 +0300

    feat(staging): Add staging superstore

---
+++ src/models/staging/stg_superstore.sql
@@ -0,0 +1,26 @@
+with superstore as (
+    select
+        "Row ID" as row_id,
+        "Order ID" as order_id,
+        "Order Date" as order_date,
+        "Ship Date" as ship_date,
+        "Ship Mode" as ship_mode,
+        "Customer ID" as customer_id,
+        "Customer Name" as customer_name,
+        "Segment" as segment,
+        "Country" as country,
+        "City" as city,
+        "State" as state,
+        "Postal Code" as postal_code,
+        "Region" as region,
+        "Product ID" as product_id,
+        "Category" as category,
+        "Sub-Category" as subcategory,
+        "Product Name" as product_name,
+        "Sales" as sales,
+        "Quantity" as quantity,
+        "Discount" as discount,
+        "Profit" as profit
+    from public.superstore
+)
+select * from superstore
