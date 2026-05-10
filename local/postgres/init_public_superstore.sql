6a5e3f9ff62f4f3dd392224f856b9aec75556b7e
ryanhja <rayanstive9@gmail.com>
2026-05-10 18:37:14 +0300

    feat(deploy): Add init database

---
+++ local/postgres/init_public_superstore.sql
@@ -0,0 +1,23 @@
+create table if not exists public.superstore (
+	"Row ID" int4 NULL,
+	"Order ID" varchar(50) NULL,
+	"Order Date" varchar(50) NULL,
+	"Ship Date" varchar(50) NULL,
+	"Ship Mode" varchar(50) NULL,
+	"Customer ID" varchar(50) NULL,
+	"Customer Name" varchar(50) NULL,
+	"Segment" varchar(50) NULL,
+	"Country" varchar(50) NULL,
+	"City" varchar(50) NULL,
+	"State" varchar(50) NULL,
+	"Postal Code" int4 NULL,
+	"Region" varchar(50) NULL,
+	"Product ID" varchar(50) NULL,
+	"Category" varchar(50) NULL,
+	"Sub-Category" varchar(50) NULL,
+	"Product Name" varchar(128) NULL,
+	"Sales" float4 NULL,
+	"Quantity" int4 NULL,
+	"Discount" float4 NULL,
+	"Profit" float4 NULL
+);
