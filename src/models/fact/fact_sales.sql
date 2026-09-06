a111ee723d9247e448047e7418f4fd747985c014
ryanhja <rayanstive9@gmail.com>
2026-09-06 13:20:46 +0300

    feat(models): update fact_sales.sql

--- src/models/fact/fact_sales.sql
+++ src/models/fact/fact_sales.sql
@@ -1,20 +1,34 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='order_date',
+    incremental_strategy='delete+insert'
+  )
+}}
 with superstore as (
     select
-        cast(to_char(to_date(ss.order_date, 'MM/DD/YYYY'), 'YYYYMMDD') as int) as fk_date,
+        to_date(ss.order_date, 'MM/DD/YYYY') as order_date,
+        sm.id as ship_id,
+        to_date(ss.ship_date, 'MM/DD/YYYY') As ship_date,
         ss.customer_id,
-        dl.location_id,
-        ss.product_id
+        ss.product_id,
+        ss.sales,
+        ss.quantity,
+        ss.discount,
+        ss.profit
     from {{ ref("stg_superstore") }} ss
-    inner join {{ ref("dim_location") }} dl on
-        COALESCE(ss.country, '') = COALESCE(dl.country, '') AND
-        COALESCE(ss.region, '') = COALESCE(dl.region, '') AND
-        COALESCE(ss.state, '') = COALESCE(dl.state, '') AND
-        COALESCE(ss.city, '') = COALESCE(dl.city, '') AND
-        COALESCE(ss.postal_code, 0) = COALESCE(dl.postal_code, 0)
+    join dim.dim_ship_mode sm
+        on coalesce(ss.ship_mode, '') = coalesce(sm.ship_mode, '')
 )
 select
-    fk_date,
+    order_date,
+    ship_id,
+    ship_date,
     customer_id,
-    location_id,
-    product_id
+    product_id,
+    sales,
+    quantity,
+    discount,
+    profit
 from superstore
+order by order_date
